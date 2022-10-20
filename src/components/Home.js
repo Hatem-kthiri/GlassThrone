2d71c11368a1b48923b5aa1d78719145f0f82675
Hatem-kthiri <hatemkthiri1997@gmail.com>
2022-10-20 16:48:50 +0100

    Portfolio GlassThrone

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Typed from "react-typed";
-import "./style.css";
 const Home = () => {
     return (
         <section id="home" className="active">
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./style.css";
 const Portfolio = () => {
     // eslint-disable-next-line
     const [portfolioItem, setPortfolioItem] = useState([
